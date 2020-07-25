d24ff0935468660a892f2bff7414637978a56b19
Igor Pereira <68043835+i-grr@users.noreply.github.com>
2020-07-25 00:41:27 -0300

    Adding effect to the navigation bar when scrolling.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Se o usuário realizar a rolagem, trocar o menu transparente pelo colorido
+
+  window.onscroll = function() { scrollFunction()};
+
+  function scrollFunction() {
+
+    // Se rolagem maior do que 50px
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      // Definir header black
+      document.getElementById("menu").classList.add('header-black');
+      document.getElementById("menu").classList.remove('header-transparent');
+    // Se não
+    } else {
+      // Definir header transparente
+      document.getElementById("menu").classList.add('header-transparent');
+      document.getElementById("menu").classList.remove('header-black');
+    }
+  }
