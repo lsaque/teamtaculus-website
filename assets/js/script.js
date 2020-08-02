a8f96e8a72e8dbd65bc0329ad816625ebb0980ac
Isaque Jose <isaquetoonix245@gmail.com>
2020-08-02 02:19:53 -0300

    Added "Patrocinadores" section and responsivity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
 // Se o usuário realizar a rolagem, trocar o menu transparente pelo colorido
 
-  window.onscroll = function() { scrollFunction()};
-  window.onload = function() { scrollFunction()};
+window.onscroll = function () {
+  scrollFunction()
+};
+window.onload = function () {
+  scrollFunction()
+};
 
-  function scrollFunction() {
+function scrollFunction() {
 
-    // Se rolagem maior do que 50px
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      // Definir header black
-      document.getElementById("menu").classList.add('header-black');
-      document.getElementById("menu").classList.remove('header-transparent');
+  // Se rolagem maior do que 50px
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    // Definir header black
+    document.getElementById("menu").classList.add('header-black');
+    document.getElementById("menu").classList.remove('header-transparent');
     // Se não
-    } else {
-      // Definir header transparente
-      document.getElementById("menu").classList.add('header-transparent');
-      document.getElementById("menu").classList.remove('header-black');
-    }
+  } else {
+    // Definir header transparente
+    document.getElementById("menu").classList.add('header-transparent');
+    document.getElementById("menu").classList.remove('header-black');
   }
+}
--- assets/js/slick-slider.js
+++ assets/js/slick-slider.js
@@ -1,32 +1,31 @@
 // Incializando Slider
-  $(document).ready(function(){
-    $('.slick-carousel').slick({
-      centerMode: true,
-      centerPadding: '60px',
-      dots: true,
-      arrows: false,
-      infinite: false,
-      slidesToShow: 1,
-      variableWidth: true,
-      focusOnSelect: true,
-      initialSlide: 2,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
-          }
+$(document).ready(function () {
+  $('.slick-carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    dots: true,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 1,
+    variableWidth: true,
+    focusOnSelect: true,
+    initialSlide: 2,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
         }
-      ]
-    });
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
+});
